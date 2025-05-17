fbf30a5333ca3e2e344650cb9c36ca76f81e56ad
PhillipA-Asiimwe <211544632+AmanyaPhillip@users.noreply.github.com>
2025-05-17 18:50:28 -0400

    BankApp Commit

---
+++ Software Development/Java/BankApp/src/main/java/com/example/bankapp/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.example.bankapp.repository;
+import com.example.bankapp.model.Account;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
+
+   
+
+}
---
+++ Software Development/Java/BankApp/src/main/java/com/example/bankapp/service/AccountService.java
@@ -0,0 +1,58 @@
+package com.example.bankapp.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.example.bankapp.model.Account;
+import com.example.bankapp.model.Transaction;
+import com.example.bankapp.repository.AccountRepository;
+import com.example.bankapp.repository.TransactionRepository;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Account findAccountByUsername(String username) {
+        return accountRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Account not found"));
+    }
+
+    public Account registerAccount(String username, String password) {
+        if (accountRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(passwordEncoder.encode(password));
+        account.setBalance(BigDecimal.ZERO);
+        return accountRepository.save(account);
+    }
+
+    public void deposit(Account account, BigDecimal amount){
+        account.setBalance(account.getBalance().add(amount));
+        accountRepository.save(account);
+
+        Transaction transaction = new Transaction(amount, "DEPOSIT", LocalDateTime.now(), account);
+        transactionRepository.save(transaction);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+    }
+
+}
