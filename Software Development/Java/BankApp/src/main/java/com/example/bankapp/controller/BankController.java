5d5313c939241192fa5faba736a949a5ac8ef26c
PhillipA-Asiimwe <211544632+AmanyaPhillip@users.noreply.github.com>
2025-05-17 20:05:09 -0400

    Config Finalization

--- Software Development/Java/BankApp/src/main/java/com/example/bankapp/controller/BankController.java
+++ Software Development/Java/BankApp/src/main/java/com/example/bankapp/controller/BankController.java
@@ -46,7 +46,7 @@ public class BankController {
     }
 
     @GetMapping("/login")
-    public String showLoginForm(){
+    public String login(){
         return "login";
     }
 
@@ -79,6 +79,21 @@ public class BankController {
         Account account = accountService.findAccountByUsername(username);
         model.addAttribute("transactions", accountService.getTransactionHistory(account));
         return "transactions";
+    }
+
+    @PostMapping("/transfer")
+    public String transfer(@RequestParam String toUsername, @RequestParam BigDecimal amount, Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account fromAccount = accountService.findAccountByUsername(username);
+        try {
+            accountService.transferAmount(fromAccount, toUsername, amount);
+        
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("account", fromAccount);
+            return "dashboard";
+        }
+        return "redirect:/dashboard";
     }
 
 }
